d80a939b760e6c3ad009986acb28d8fda66d6791
Egilson Cabral <egilsoncabral@hotmail.com>
2019-08-27 18:39:01 -0300

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Form(
+      child: ListView(
+        children: <Widget>[
+          Text("Login")
+        ],
+      ),
+    );
+  }
+}
